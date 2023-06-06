1044698561d0974254cfed852c03cfef6c090eeb
gedearya <igd.ad.pratama@gmail.com>
2023-06-06 22:08:13 +0700

    define BASE_URL, BASE_IMAGE_URL, API_KEY, BASE_IMAGE_URL_SMALL in tmdb.js

---
+++ src/api/tmdb.js
@@ -0,0 +1,6 @@
+const BASE_URL = "https://api.themoviedb.org/3";
+const API_KEY = "cd23c6a2cd401bc41eae6f83915084c6";
+const BASE_IMAGE_URL = "http://image.tmdb.org/t/p/original";
+const BASE_IMAGE_URL_SMALL = "http://image.tmdb.org/t/p/w500";
+
+export { BASE_URL, BASE_IMAGE_URL, API_KEY, BASE_IMAGE_URL_SMALL };
