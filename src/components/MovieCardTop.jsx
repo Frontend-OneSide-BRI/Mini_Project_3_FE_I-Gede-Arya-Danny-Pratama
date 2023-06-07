993d6137cf0f349d6f7208c35179f5a2cc4cf2f5
gedearya <igd.ad.pratama@gmail.com>
2023-06-08 05:53:56 +0700

    add formatDate on MovieCardTop.jsx

--- src/components/MovieCardTop.jsx
+++ src/components/MovieCardTop.jsx
@@ -1,4 +1,15 @@
-function MovieCardTop() {
+function MovieCardTop({ item, top }) {
+  const formatDate = (releaseDate) => {
+    const date = new Date(releaseDate);
+    const formatDate = date.toLocaleDateString("en-us", {
+      year: "numeric",
+      month: "short",
+      day: "numeric",
+    });
+
+    return formatDate;
+  };
+
   return (
 
   );
