53bafbcfe9534ec6a7bc760b6a62ce2275eff4c6
gedearya <igd.ad.pratama@gmail.com>
2023-06-08 06:20:52 +0700

    add return to dispay in MovieCard.jsx

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,3 +1,6 @@
+import { Link } from "react-router-dom";
+import { BASE_IMAGE_URL_SMALL } from "../api/tmdb";
+
 function MovieCard({ item, movie = true }) {
   const formatDate = (releaseDate) => {
     const date = new Date(releaseDate);
@@ -11,7 +14,28 @@ function MovieCard({ item, movie = true }) {
   };
 
   return (
- 
+    <Link to={movie ? `/movie/${item.id}` : `/series/${item.id}`}>
+      <div className="movie-card flex flex-col gap-2 cursor-pointer">
+        <div className="rounded-md bg-red-700 h-72 w-48">
+          <img
+            src={
+              item.poster_path
+                ? `${BASE_IMAGE_URL_SMALL}${item.poster_path}`
+                : "srcassetsimagesimage_not_found.png"
+            }
+            alt={item.title}
+            className="h-full w-full movie rounded-md object-cover object-center"
+          />
+        </div>
+        <p className="movie-card__title font-medium text-lg">
+          {movie ? item.title : item.name}
+        </p>
+        <p className="text-sm font-light">
+          {(item.release_date || item.first_air_date) &&
+            formatDate(movie ? item.release_date : item.first_air_date)}
+        </p>
+      </div>
+    </Link>
   );
 }
 
