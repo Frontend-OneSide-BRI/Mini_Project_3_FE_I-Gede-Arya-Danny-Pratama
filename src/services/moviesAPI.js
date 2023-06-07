661c43a7a4c4c8d7fb5cd8ab865758c1b5dd5cc9
gedearya <igd.ad.pratama@gmail.com>
2023-06-07 13:04:54 +0700

    add file moviesAPI in services and install redux toolkit

---
+++ src/services/moviesAPI.js
@@ -0,0 +1,16 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { BASE_URL, API_KEY } from "../apis/tmdb";
+
+export const moviesApi = createApi({
+  reducerPath: "moviesApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: BASE_URL,
+  }),
+  endpoints: (build) => ({
+    popularMovie: build.query({
+      query: () => `/discover/movie?api_key=${API_KEY}&sort_by=popularity.desc`,
+    }),
+  }),
+});
+
+export const { usePopularMovieQuery } = moviesApi;
