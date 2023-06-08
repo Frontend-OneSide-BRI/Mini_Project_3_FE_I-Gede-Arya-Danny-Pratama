0d420ec680d818685251511067844f2d36118ff4
gedearya <igd.ad.pratama@gmail.com>
2023-06-08 12:51:40 +0700

    add data Movie Recommendation in DetailMovie.jsx

--- src/pages/DetailMovie.jsx
+++ src/pages/DetailMovie.jsx
@@ -2,17 +2,24 @@ import { useParams } from "react-router-dom";
 import {
   useDetailMovieQuery,
   useMovieSimilarQuery,
+  useMovieRecommendationsQuery,
 } from "../services/moviesAPI";
 
 function DetailMovie() {
   const { id } = useParams();
   const { data, error, isLoading } = useDetailMovieQuery(`${id}`);
+
   const {
     data: dataMovieSim,
     error: errorMovieSim,
     isLoading: isLoadingMovieSim,
   } = useMovieSimilarQuery(`${id}`);
 
+  const {
+    data: dataMovieRec,
+    error: errorMovieRec,
+    isLoading: isLoadingMovieRec,
+  } = useMovieRecommendationsQuery(`${id}`);
   return (
     <>
       <h1>INI DETAIL PAGE MOVIE</h1>
