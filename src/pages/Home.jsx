e12f1bf8877336de64b7e2c3bef3aa10d22f7d9a
gedearya <igd.ad.pratama@gmail.com>
2023-06-08 07:05:14 +0700

    add useEffect for scroll and data Popular movie in Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,16 @@
+import { useEffect } from "react";
+import { usePopularMovieQuery } from "../services/moviesAPI";
+
 function Home() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
+  const {
+    data: dataPopular,
+    error: errorPopular,
+    isLoading: isLoadingPopular,
+  } = usePopularMovieQuery();
   return <div>INI HOME</div>;
 }
 
