7ddb76c0b6aa1f754b4e51e2b60bef3b35bba760
gedearya <igd.ad.pratama@gmail.com>
2023-06-08 12:31:42 +0700

    add useParams for DetailMovie.jsx

--- src/pages/DetailMovie.jsx
+++ src/pages/DetailMovie.jsx
@@ -1,4 +1,10 @@
+import { useParams } from "react-router-dom";
+import { useDetailMovieQuery } from "../services/moviesAPI";
+
 function DetailMovie() {
+  const { id } = useParams();
+  const { data, error, isLoading } = useDetailMovieQuery(`${id}`);
+
   return (
     <>
       <h1>INI DETAIL PAGE MOVIE</h1>
