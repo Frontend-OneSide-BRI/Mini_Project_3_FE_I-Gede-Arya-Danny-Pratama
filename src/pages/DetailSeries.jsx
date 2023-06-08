7352885453b418771073f4bb9b84af7bb2c18af1
gedearya <igd.ad.pratama@gmail.com>
2023-06-08 16:26:26 +0700

    add DetailSeriesQuery and useParams into DetailSeries.jsx

--- src/pages/DetailSeries.jsx
+++ src/pages/DetailSeries.jsx
@@ -1,4 +1,8 @@
+import { useDetailSeriesQuery } from "../services/moviesAPI";
+
 function DetailSeries() {
+  const { id } = useParams();
+  const { data, error, isLoading } = useDetailSeriesQuery(`${id}`);
   return (
     <>
       <h1>INI HALAMAN DETAIL SERIES</h1>
