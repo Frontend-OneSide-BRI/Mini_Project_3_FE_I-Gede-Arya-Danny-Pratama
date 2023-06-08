58f259bb401db2ba4423c6ae5e9759a8593bdfc2
gedearya <igd.ad.pratama@gmail.com>
2023-06-08 08:26:24 +0700

    add file Login.jsx in pages folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import Footer from "./components/Footer";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <>
       <Header />
       <div className="mt-20"></div>
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
       <Footer />
     </>
   );
