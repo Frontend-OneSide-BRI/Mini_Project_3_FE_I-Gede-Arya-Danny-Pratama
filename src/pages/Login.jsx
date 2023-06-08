db1a7cd920503f430c0467586ef293954ddd9724
gedearya <igd.ad.pratama@gmail.com>
2023-06-08 08:37:11 +0700

    add function to show/hide password in Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,13 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 function Login() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
+  const [isPasswordShown, setIsPasswordShown] = useState(false);
+
   return (
     <div className="flex flex-col items-center my-32 gap-10">
       <div className="flex flex-col gap-3 w-full max-w-md">
@@ -16,16 +23,26 @@ function Login() {
           />
           <div className="relative">
             <input
-              type="password"
+              type={isPasswordShown ? "text" : "password"}
               name="password"
               placeholder="Password"
               required
               className="bg-zinc-900 border border-zinc-500 p-3 rounded-sm w-full"
             />
             <div className="absolute top-0 right-2 h-full flex items-center">
-              <img alt="hide" className="p-2 cursor-pointer bg-zinc-900" />
+              <img
+                onClick={() => setIsPasswordShown(!isPasswordShown)}
+                src={
+                  isPasswordShown
+                    ? "src/assets/images/ic_eyeoff.svg"
+                    : "src/assets/images/ic_eye.svg"
+                }
+                alt="hide"
+                className="p-2 cursor-pointer bg-zinc-900"
+              />
             </div>
           </div>
+
           <button
             type="submit"
             className="bg-red-700 hover:bg-red-800 p-3 rounded-sm"
