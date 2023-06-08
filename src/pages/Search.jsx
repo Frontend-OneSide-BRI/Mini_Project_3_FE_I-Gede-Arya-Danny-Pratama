5f1b6f88e2d5f0c99961e4aaa4539ea4eb2639b1
gedearya <igd.ad.pratama@gmail.com>
2023-06-08 17:34:47 +0700

    add SearchMovieQuery and params into Search.jsx

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,4 +1,13 @@
+import { useParams } from "react-router-dom";
+import { useSearchMoviesQuery } from "../services/moviesAPI";
+
 function Search() {
+  window.scrollTo(0, 0);
+  const { query, page } = useParams();
+  const { data, error, isLoading } = useSearchMoviesQuery({
+    query: query,
+    page: page,
+  });
   return (
     <>
       <h1>INI PAGE HASIL SEARCH</h1>
