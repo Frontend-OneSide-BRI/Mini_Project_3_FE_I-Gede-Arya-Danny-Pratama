dfc03f740b4bbfe0f8a9ebafc849e4a909b90c32
gedearya <igd.ad.pratama@gmail.com>
2023-06-08 16:38:06 +0700

    add data SerriesSimiliar Movie into DetailSeries.jsx

--- src/pages/DetailSeries.jsx
+++ src/pages/DetailSeries.jsx
@@ -1,8 +1,18 @@
-import { useDetailSeriesQuery } from "../services/moviesAPI";
+import {
+  useDetailSeriesQuery,
+  useSeriesSimilarQuery,
+} from "../services/moviesAPI";
+import { useParams } from "react-router-dom";
 
 function DetailSeries() {
   const { id } = useParams();
   const { data, error, isLoading } = useDetailSeriesQuery(`${id}`);
+
+  const {
+    data: dataSeriesSim,
+    error: errorSeriesSim,
+    isLoading: isLoadingSeriesSim,
+  } = useSeriesSimilarQuery(`${id}`);
   return (
     <>
       <h1>INI HALAMAN DETAIL SERIES</h1>
