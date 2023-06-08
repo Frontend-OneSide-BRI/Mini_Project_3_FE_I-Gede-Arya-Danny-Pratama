c12865ad98857ccb9b195f769d2d24223ac7cc88
gedearya <igd.ad.pratama@gmail.com>
2023-06-08 09:07:00 +0700

    add function handleSubmit and connect with firebase

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,19 +1,53 @@
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../auth/firebase";
 
 function Register() {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
 
+  const [errorMessage, setErrorMessage] = useState("");
   const [isPasswordShown, setIsPasswordShown] = useState(false);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    const email = data.get("email");
+    const password = data.get("password");
+
+    try {
+      const { user } = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      navigate("/");
+      return user;
+    } catch (error) {
+      let errMessage = "There was an error";
+      const sanitizeErrMsg = error.message.split("/")[1].split(")")[0];
+
+      if (sanitizeErrMsg === "weak-password") {
+        errMessage = "Password should be at least 6 characters";
+      }
+
+      if (sanitizeErrMsg === "email-already-in-use") {
+        errMessage = "Email already in use";
+      }
+
+      setErrorMessage(`* ${errMessage}`);
+    }
+  };
 
   return (
     <div className="flex flex-col items-center my-32 gap-10">
       <div className="flex flex-col gap-3 w-full max-w-md">
         <p className="text-2xl mb-4 text-center">REGISTER</p>
 
-        <form className="flex flex-col gap-3">
+        <form onSubmit={handleSubmit} className="flex flex-col gap-3">
           <input
             type="email"
             name="email"
@@ -42,6 +76,8 @@ function Register() {
               />
             </div>
           </div>
+
+          {errorMessage && <p className="text-red-400">{errorMessage}</p>}
 
           <button
             type="submit"
